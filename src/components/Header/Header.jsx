00e1e9231522b76fcfe078646bbf7f46886b4c0d
Asmaul-Shahana <jucse28.360@gmail.com>
2023-05-03 03:30:05 +0600

    see all jobs button commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,26 +9,23 @@ const Header = () => {
         <div className='backgroundcolor px-6 py-6 md:px-28 md:pt-12'>
             <div className='md:flex justify-between items-center'>
                 <h2 className='font-bold text-2xl'>TechNexus</h2>
-                <div>
-                    <nav>
-                        <div onClick={() => setOpen(!open)} className='md:hidden'>
-                            <span>
-                                {
-                                    open === true ? <XMarkIcon className="h-7 w-7 text-[#9873FF]" />
-                                        : <Bars3Icon className="h-7 w-7 text-[#9873FF]" />
-                                }
-                            </span>
-                        </div>
-                        <ul className={`md:flex justify-between absolute md:static duration-500 ${open ? 'top-32' : '-top-96'} bg-violet-100 pl-6 x-2 y-2`}>
-                            <ActiveLink to="/">Home</ActiveLink>
-                            <ActiveLink to="/statistics">Statistics</ActiveLink>
-                            <ActiveLink to="/jobs">Applied Jobs</ActiveLink>
-                            <ActiveLink to="/blogs">Blog</ActiveLink>
-                        </ul>
-                    </nav>
-                </div>
-                <button className='flex flex-start gap-3 text-white font-semibold bg-gradient-to-r from-[#7E90FE] to-[#9873FF] px-4 py-3 rounded-lg
-'>Start Applying</button>
+                <nav>
+                    <div onClick={() => setOpen(!open)} className='md:hidden'>
+                        <span>
+                            {
+                                open === true ? <XMarkIcon className="h-7 w-7 text-[#9873FF]" />
+                                    : <Bars3Icon className="h-7 w-7 text-[#9873FF]" />
+                            }
+                        </span>
+                    </div>
+                    <div className={`flex gap-7 justify-between absolute md:static duration-500 ${open ? 'top-36' : '-top-96'} pl-6 x-2 y-2`}>
+                        <ActiveLink to="/">Home</ActiveLink>
+                        <ActiveLink to="/statistics">Statistics</ActiveLink>
+                        <ActiveLink to="/jobs">Applied Jobs</ActiveLink>
+                        <ActiveLink to="/blogs">Blog</ActiveLink>
+                    </div>
+                </nav>
+                <button className='flex flex-start gap-3 text-white font-semibold bg-gradient-to-r from-[#7E90FE] to-[#9873FF] px-4 py-3 rounded-lg'>Start Applying</button>
             </div>
         </div>
     );
